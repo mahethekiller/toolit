bc766b18f718672c3d29a8d27183149112f7129f
mahethekiller <abc@gmail.com>
2025-09-22 18:41:42 +0530

    pages faq desc

--- resources/views/partials/faqs.blade.php
+++ resources/views/partials/faqs.blade.php
@@ -2,22 +2,24 @@
      <div class="col-md-12 mb-4 card shadow border-0 rounded-4">
          <h2 class="mt-5 mb-4 text-center fw-bold">Frequently Asked Questions</h2>
 
-<div class="accordion" id="faqAccordion">
+<div class="accordion" id="faqAccordion" itemscope itemtype="https://schema.org/FAQPage">
     @foreach ($faqs as $faq)
-        <div class="accordion-item mb-3">
+        <div class="accordion-item mb-3" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
             <div class="card shadow-sm rounded">
-                <div class="card-header p-0" id="faqHeading{{ $loop->index }}">
+                <div class="card-header p-0">
                     <button class="accordion-button {{ $loop->first ? '' : 'collapsed' }}" type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#faqCollapse{{ $loop->index }}"
                         aria-expanded="{{ $loop->first ? 'true' : 'false' }}"
-                        aria-controls="faqCollapse{{ $loop->index }}">
+                        aria-controls="faqCollapse{{ $loop->index }}"
+                        itemprop="name">
                         {{ $faq->question }}
                     </button>
                 </div>
                 <div id="faqCollapse{{ $loop->index }}" class="accordion-collapse collapse {{ $loop->first ? 'show' : '' }}"
-                    aria-labelledby="faqHeading{{ $loop->index }}" data-bs-parent="#faqAccordion">
-                    <div class="card-body text-muted">
+                    aria-labelledby="faqHeading{{ $loop->index }}" data-bs-parent="#faqAccordion"
+                    itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                    <div class="card-body text-muted" itemprop="text">
                         {!! $faq->answer !!}
                     </div>
                 </div>
@@ -25,6 +27,7 @@
         </div>
     @endforeach
 </div>
+
 
 
      </div>
