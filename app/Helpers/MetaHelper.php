5d9803919b8e6f82b57b2fcbc94149db51e0c328
mahethekiller <abc@gmail.com>
2026-03-25 17:05:22 +0530

    Fixes warranty

--- app/Helpers/MetaHelper.php
+++ app/Helpers/MetaHelper.php
@@ -5,7 +5,7 @@ use App\Models\Seo;
 if (!function_exists('getSeo')) {
     function getSeo($url = null)
     {
-        echo $url = $url ?? request()->url(); // e.g., /tools/case-converter
+        $url = $url ?? request()->url(); // e.g., /tools/case-converter
         return Seo::where('url', $url)->first();
     }
 }
